6073134087e710ba74f2be4c5215fa57de01bd63
giovanniPi997 <giovannipietrucci27@gmail.com>
2022-11-21 11:26:37 +0100

    Taglio del dendogramma per ottenere il numero di cluster

--- Scalable/src/main/scala/main.scala
+++ Scalable/src/main/scala/main.scala
@@ -2,46 +2,12 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.Level
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions.lit
 
 import math.pow
 import java.io._
 import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 import scala.collection.immutable.Nil.combinations
-
-
-/*
-object test
-{
-  // Main Method
-  case class Country(index: String, co2: String,gdp: String)
-
-  def main(args: Array[String]): Unit = {
-    var conf = new SparkConf().setAppName("Read CSV File").setMaster("local[*]")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
-
-    ///val textRDD = sc.textFile("2010_co2_gdp.csv")
-    //println(textRDD.foreach(println)
-
-    var empRdd = textRDD.map {
-      line =>
-        val col = line.split(",")
-        Country(col(0), col(1), col(2))
-    }
-
-    val empDF = empRdd.toDF()
-    empDF.show()
-
-    var df2 = empDF.withColumn("co2", empDF("co2").cast("int"))
-    df2 = empDF.withColumn("gdp", empDF("gdp").cast("float"))
-    df2.show()
-    //df2.filter(df2("year") === "1960").show(true)
-  }
-}
-*/
-
-
 
 object test
 {
@@ -90,7 +56,7 @@ object test
     val empRddProva = empRddZipped.filter(_._2 > 6).filter(_._2 < 16).keys // Creo un dataframe per le prove
     val empDFProva = empRddProva.toDF()
     empDFProva.show()
-
+    println(empDFProva)
     //creo gli indici iniziali da 0 a len(df)
     var indici : List[Int] = List.range(0, empDFProva.count().toInt)
     //println(indici) List(0, 1, 2, 3, 4, 5, 6, 7, 8)
@@ -103,7 +69,10 @@ object test
     // println(empDFProva.rdd.take(1).last(3) , empDFProva.rdd.take(1).last(4))
     val col_co2 = empDFProva.select("co2").map(_.getString(0)).collectAsList.map(_.toDouble).toList
     val col_gdp = empDFProva.select("gdp").map(_.getString(0)).collectAsList.map(_.toDouble).toList
+
     val xy_zip = col_co2 zip col_gdp
+
+    println(xy_zip)
     //println(xy_zip(3)._1)
     //println(xy_zip(3)._2)
 
@@ -119,14 +88,12 @@ object test
     for( a <- 1 to original_lenght-1) {
       var combinazioni = indici.filter(_!=(-1)).combinations(2).toList
       //combinazioni = combinazioni.filter(element => element(0)!=(-1) && element(1)!=(-1))
-      println("-----------------")
-      println(combinazioni)
-      println("-----------------")
+      //NON ELIMINARE println("-----------------")
       //mapping della lista di combinazioni con l'errore quadratico associato
       val error_list = combinazioni.map(distance(xy_zip, _, dizionario))
 
-      println(combinazioni(error_list.indexOf(error_list.min)))
-      println(indici)
+      //NON ELIMINARE--------println("MSE tuple: ", combinazioni(error_list.indexOf(error_list.min)))
+
       // Combinazione con l'errore minimo minore
       val coppia = combinazioni(error_list.indexOf(error_list.min))
       indici = indici.updated(coppia(0), -1) // List.updated(index, new_value)
@@ -135,13 +102,46 @@ object test
       indici = indici :+ indici.length
       dizionario = dizionario :+ coppia
 
-      println(indici)
-      println(dizionario)
+      //NON ELIMINARE--------println("Index : ",indici)
     }
 
 
+    println(dizionario)
+    println("ULTIMO CLUSTER: "+ dizionario.last)
 
+
+    for (i <- dizionario.last(0) to dizionario.length - 1) {
+      println("CLUSTER "+ i + " -> " + dizionario(i))
+    }
+    var cluster: List[Any] = List()
+    for(i <- dizionario.last(0) to dizionario.length-1) {
+      println("---------------------------------------")
+      println("CLUSTER NUMERO ->" + i)
+      println("CLUSTER ->" + dizionario(i))
+      // exp: è il cluster espanso attualmente analizzato
+      var exp = expand(List(i), dizionario)
+      println("CLUSTER ESPANSO -> " + exp)
+
+      if(i == dizionario.last(0)){
+        cluster = cluster :+ i
+      }else {
+        if (dizionario(i)(0) < dizionario.last(0)) {
+          cluster = cluster :+ dizionario(i)(0)
+        }
+        if ((dizionario(i)(1) < dizionario.last(0))) {
+          cluster = cluster :+ dizionario(i)(1)
+        }
+      }
+
+    }
+    println(cluster)
+    println("NUMERO DI CLUSTER: "+ cluster.length)
   }
+    /*
+    val df = empDFProva.withColumn("Label",lit("newValue"))//empDFProva.withColumn("Label", empDFProva("gdp") + 1) // -- OK
+    df.show()
+    */
+
 
 
 
@@ -184,16 +184,13 @@ object test
        //(0,1)  (0,2) ,.... (5,6) combinazioni
        //(3,5)-> 3,(0,1) -> 3,0,1 flat
 
-       var all_x : List[Double] = List()
-       var all_y : List[Double] = List()
-       var X, Y : Double = 0
-       var points_new= expand(points,dizionario)
-    println("expand ", expand(points,dizionario))
-    println("ooo",points_new)
+      var all_x : List[Double] = List()
+      var all_y : List[Double] = List()
+      var X, Y : Double = 0
+      var points_new= expand(points,dizionario)
       val n = points_new.length
       for(i <- points_new){
 
-        //println(dataFrame(i)._1)
         all_x = all_x :+ dataFrame(i)._1 //CO2
         all_y = all_y :+ dataFrame(i)._2 //GDP
 
@@ -210,29 +207,19 @@ object test
       for (i <- 0 to n-1) {
         point = Point(all_x(i),all_y(i))
         error_square = error_square + ptMedio.error_square_fun(point)
-
       }
       error_square
       }
 
-  def cast[A <: AnyRef : Manifest](a : Any) : A
-  = manifest[A].erasure.cast(a).asInstanceOf[A]
 
   def expand(points: List[Int], dizionario: List[List[Int]]): List[Int] = {
     var points_extend : List[Int] = List()
-    //println("PUNTI EXTEND:  ", points," Original Lenght: ",original_lenght)
     if (points.max<original_lenght){
       points
     }else{
       for(i <- points){ //points= List(1,6)   i=1  i=6
-        //println(i)
-
-        //temp_list.isEmpty
-
         if(dizionario(i).length > 1){
-
           var temp_list: List[Int] = dizionario(i)
-          println(temp_list)
           points_extend = points_extend:+ temp_list(0)
           points_extend = points_extend:+ temp_list(1)
           points_extend = expand(points_extend,dizionario)
@@ -249,7 +236,6 @@ object test
         }
         */
       }
-      println("FINALE ", points_extend)
       points_extend
     }
   }
@@ -265,22 +251,5 @@ object test
   }
   */
 
-    /*
-     val pt1 = Point(1, 2)  //qui bisogna prendere le coordinate dal df e non ho capito come si fa
-     val pt2 = Point(3, 4)
-     val x = (pt1.x + pt2.x) / 2
-     val y = (pt1.y + pt2.y) / 2
-
-     val ptMedio = Point(x,y)
-     //val dist= ptMedio.distance(pt1)
-     val dist1= pt1.distance(pt2)
-
-     dist1
-  */
-       //println(dist,dist1)
-
-
-
-  //} stavo iniziando il calcolo dell'errore
 
 }
