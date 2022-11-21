266b151d5ff7d298c67152eacbbac00a39ebff03
giovanniPi997 <giovannipietrucci27@gmail.com>
2022-11-21 13:07:01 +0100

    create ordered label list column

--- Scalable/src/main/scala/main.scala
+++ Scalable/src/main/scala/main.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.Level
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.functions.{lit, typedLit}
 
 import math.pow
 import java.io._
@@ -53,7 +53,7 @@ object test
     for (anno <- 1959 to 2013) mapAnnoDF += (anno -> df2.filter(df2("year") === anno))
     //mapAnnoDF(2003).show()
 
-    val empRddProva = empRddZipped.filter(_._2 > 6).filter(_._2 < 16).keys // Creo un dataframe per le prove
+    val empRddProva = empRddZipped.filter(_._2 > 0).filter(_._2 < 20).keys // Creo un dataframe per le prove
     val empDFProva = empRddProva.toDF()
     empDFProva.show()
     println(empDFProva)
@@ -113,7 +113,7 @@ object test
     for (i <- dizionario.last(0) to dizionario.length - 1) {
       println("CLUSTER "+ i + " -> " + dizionario(i))
     }
-    var cluster: List[Any] = List()
+    var cluster: List[Int] = List()
     for(i <- dizionario.last(0) to dizionario.length-1) {
       println("---------------------------------------")
       println("CLUSTER NUMERO ->" + i)
@@ -136,12 +136,70 @@ object test
     }
     println(cluster)
     println("NUMERO DI CLUSTER: "+ cluster.length)
+    //Cluster: List(14, 6, 12)
+    // Expand(14) -> 0, 4, 5, 1, 2, 3 -----> LABEL 1
+    // Expand(6) ->  6                -----> LABEL 2
+    // Expand(12) -> 7, 8             -----> LABEL 3
+
+    var single_expanded: List[Int] = List()
+    var label_expanded: List[Int] = List()
+    var total_expanded: List[Product with scala.Serializable] = List()
+    var a:List[(Int, Int)] = List()
+
+    for (i <-0 to cluster.length-1){
+      single_expanded = expand(List(cluster(i)),dizionario)
+      label_expanded = List.fill[Int](single_expanded.length)(i)
+      total_expanded = total_expanded :+  (single_expanded zip label_expanded)
+    }
+    //mergedList.seq.head._1.someField
+    //total_expanded = total_expanded.sorted
+   // total_expanded.seq.head._1._1
+
+
+
+
+    // VERSIONE ALTERNATIVA "alla maniera di Scala"
+    val cluster_expanded : List[List[Int]] = cluster.map(x => expand(List(x), dizionario))
+    val cluster_zipped : List[List[(Int, Int)]] = cluster_expanded.zipWithIndex.map(x => x._1.zip(List.fill[Int](x._1.length)(x._2)))
+    val cluster_flat : List[(Int, Int)] = cluster_zipped.flatten.sortBy(_._1)
+    val label : List[Int] = cluster_flat.map(_._2)
+    val label_DF = label.toDF()
+    label_DF.show()
+    val label_column = label_DF("value")
+    println("DANIELE================", label_column)
+
+    //label_DF.withColumn("num", List.range(1, original_lenght))
+
+    //val df = empDFProva.withColumn("value", label_column.cast("int")) //empDFProva.withColumn("Label", empDFProva("gdp") + 1) // -- OK
+   // val df2 = empDFProva.withColumn("value", typedLit(label.foreach {_})) }
+
+
+
+
+
+   /* var i=0
+    def customColumnVal( label_column: List[Int]):{
+      var rd = label_column.asDict()
+      rd["Label"]=label_column(i)
+      i = i+1
+      new_row=Row(**rd)
+      return new_row
+}
+
+    //convert DF to RDD
+      df_rdd = input_dataframe.rdd
+
+    //apply new fucntion to rdd
+    output_dataframe=df_rdd.map(customColumnVal).toDF()
+      */
+
+
+
   }
     /*
     val df = empDFProva.withColumn("Label",lit("newValue"))//empDFProva.withColumn("Label", empDFProva("gdp") + 1) // -- OK
     df.show()
     */
-
 
 
 
